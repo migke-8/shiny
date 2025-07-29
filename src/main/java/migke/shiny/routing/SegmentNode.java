c1d9784a12089d5b057999b701559e06fa5c1111
migke <miguelportelabispo@gmail.com>
2025-07-29 17:53:25 -0300

    fix: fix tree based routing

--- src/main/java/migke/shiny/routing/SegmentNode.java
+++ src/main/java/migke/shiny/routing/SegmentNode.java
@@ -30,87 +30,46 @@ public class SegmentNode {
 
     public Optional<Function<HttpRequest, HttpResponse>> searchHandler(HttpMethod method, String path) {
         var leftCut = cutLeft(path);
+        if(leftCut.isEmpty()) return Optional.empty();
         var segment = cutRight(leftCut);
-        if(path.equals(leftCut) || leftCut.isEmpty())
-            return switch (this) {
-                case ParameterNode n -> n.getHandler(method, segment);
-                default -> this.getHandler(method);
-            };
-        if(leftCut.equals(segment))
-            return switch (this) {
-                case ParameterNode n -> n.searchHandler(method, path, segment);
-                default -> this.searchHandler(method, path);
-            };
         var staticNode = this.children.get(segment);
         if(staticNode != null) {
+            if(leftCut.equals(segment)) return staticNode.getHandler(method);
             var result = staticNode.searchHandler(method, leftCut);
             if(result.isPresent()) return result;
         }
         var paramNode = this.paramNode;
         if(paramNode != null) {
+            if(leftCut.equals(segment)) return paramNode.getHandler(method, segment);
             var result = paramNode.searchHandler(method, leftCut, segment);
             if(result.isPresent()) return result;
         }
         var wildCardNode = this.wildCardNode;
         if(wildCardNode != null) {
+            if(leftCut.equals(segment)) return wildCardNode.getHandler(method);
             var result = wildCardNode.searchHandler(method, leftCut);
             if(result.isPresent()) return result;
         }
         return Optional.empty();
-
-        /*return switch (node) {
-            case ParameterNode n -> n.searchHandler(method, leftCut, segment);
-            case null -> {
-                var hasEndedSearch = leftCut.isEmpty() || leftCut.equals(path);
-                yield hasEndedSearch ? this.getHandler(method) : Optional.empty();
-            }
-            default -> {
-                var matchesStatic = segment.equals(node.segment);
-                var matchesWildCard = node.segment.equals("**");
-                if(matchesStatic){
-                    var staticMatch = node.searchHandler(method, leftCut);
-                    if(staticMatch.isPresent()) yield staticMatch;
-                }
-                if(matchesWildCard) {
-                    var wildCardMatch = node.searchHandler(method, leftCut);
-                    if(wildCardMatch.isPresent()) yield wildCardMatch;
-                }
-                yield Optional.empty();
-            }
-        };*/
     }
 
     public SegmentNode add(String path) {
-        var currentNode = this;
         var leftCut = cutLeft(path);
-        var segment = "";
-
-        while (!segment.equals(leftCut)) {
-            segment = cutRight(leftCut);
-            leftCut = cutLeft(leftCut);
-            System.out.println(currentNode.segment + ": " + segment);
-            if ((!segment.equals("")) && !segment.equals("/")) {
-                var node = new SegmentNode(segment);
-                if (segment.charAt(0) == '{' && segment.charAt(segment.length() - 1) == '}') {
-                    var oldParamNode = currentNode.paramNode;
-                    if(oldParamNode != null) node = oldParamNode;
-                    else node = new ParameterNode(segment.substring(1, segment.length() - 1));
-                    currentNode.paramNode = (ParameterNode) node;
-                }
-                else if (segment.equals("**")) {
-                    var oldWildCardNode = currentNode.wildCardNode;
-                    if (oldWildCardNode != null) node = oldWildCardNode;
-                    currentNode.wildCardNode = node;
-                }
-                else {
-                    var oldStaticNode = currentNode.children.get(segment);
-                    if(oldStaticNode != null) node = oldStaticNode;
-                    currentNode.children.put(segment, node);
-                }
-                currentNode = node;
-            }
+        var segment = cutRight(leftCut);
+        System.out.println(segment);
+        if(segment.equals(path.replace("/", ""))) return this;
+        if (segment.charAt(0) == '{' && segment.charAt(segment.length() - 1) == '}') {
+            this.paramNode = this.paramNode != null ? this.paramNode : new ParameterNode(segment.substring(1, segment.length() - 1));
+            return this.paramNode.add(leftCut);
         }
-        return currentNode;
+        else if(segment.equals("**")) {
+            this.wildCardNode = this.wildCardNode != null ? this.wildCardNode : new SegmentNode(segment);
+            return this.wildCardNode.add(leftCut);
+        }
+        var oldNode = this.children.get(segment);
+        var node = oldNode != null ? oldNode : new SegmentNode(segment);
+        this.children.put(segment, node);
+        return node.add(leftCut);
     }
 
     public SegmentNode add(HttpRoute route) {
