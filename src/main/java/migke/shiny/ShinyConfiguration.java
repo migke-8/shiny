2316f09e8b71297cc9c1f661465260df04886e77
migke <miguelportelabispo@gmail.com>
2025-08-11 12:46:27 -0300

    feat: adds helper methods to the ShinyConfiguration class

--- src/main/java/migke/shiny/ShinyConfiguration.java
+++ src/main/java/migke/shiny/ShinyConfiguration.java
@@ -3,4 +3,11 @@ package migke.shiny;
 import migke.shiny.server.HttpServer;
 import migke.shiny.server.ServerConfiguration;
 
-public record ShinyConfiguration(HttpServer backend, ServerConfiguration configuration){ }
+public record ShinyConfiguration(HttpServer backend, ServerConfiguration configuration){
+    public ShinyConfiguration withBackend(HttpServer backend) {
+        return new ShinyConfiguration(backend, configuration);
+    }
+    public ShinyConfiguration withConfiguration(ServerConfiguration configuration) {
+        return new ShinyConfiguration(backend, configuration);
+    }
+}
